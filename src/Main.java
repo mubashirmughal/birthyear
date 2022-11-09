0ebd17dddde438944850edc5aabeb317c6668516
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 01:30:21 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Muhammad Mubashir Mughal C0861455
+
+import java.time.Year;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Age- ");
+        int age = sc.nextInt();
+        int year = Year.now().getValue();
+        System.out.println("Birth year is : " + (year-age));
+    }
+}
